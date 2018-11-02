6f249ba07a2dfae10a2c3b2cbadad39a3d71bd7a
Amer Karamustafic <26983729+malcodeman@users.noreply.github.com>
2018-11-02 21:29:24 +0100

    Initial app setup

---
+++ README.md
@@ -0,0 +1,1 @@
+# micro-instagram-client
---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return <div className="App">Micro instagram client</div>;
+  }
+}
+
+export default App;
