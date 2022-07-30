e8f891096f3408eb4ce7e000fb720f867d96f00e
malcodeman <26983729+malcodeman@users.noreply.github.com>
2022-07-31 00:12:12 +0200

    Updates portfolio list component completely

--- src/features/portfolios/components/PortfoliosList.js
+++ src/features/portfolios/components/PortfoliosList.js
@@ -1,73 +1,24 @@
 import React, { useState } from "react";
 import { useSelector } from "react-redux";
-import styled from "styled-components";
 import { FiList, FiGrid, FiPlus } from "react-icons/fi";
-import { ButtonGroup, Flex, IconButton, Input, Text } from "@chakra-ui/react";
+import {
+  Button,
+  ButtonGroup,
+  Flex,
+  IconButton,
+  Input,
+  Stack,
+  Text,
+} from "@chakra-ui/react";
 
 import CreateNewPortfolioModal from "./CreateNewPortfolioModal";
-import Grab from "../styles/icons/Grab";
-
-const Wrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-`;
-
-const StyledList = styled.ul`
-  list-style-type: none;
-  margin: 0;
-  padding: 0;
-`;
-
-const StyledGrab = styled(Grab)`
-  visibility: hidden;
-`;
-
-const Item = styled.li`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  padding: 10px;
-  cursor: pointer;
-  color: ${(props) => props.theme.primary};
-  border-radius: ${(props) => props.theme.borderRadius};
-  &:hover {
-    background-color: ${(props) => props.theme.backgroundSecondary};
-    ${StyledGrab} {
-      visibility: visible;
-    }
-  }
-`;
-
-const ItemText = styled.span`
-  font-size: 0.8rem;
-`;
-
-const CreateNewButton = styled.button`
-  display: flex;
-  align-items: center;
-  border: 0;
-  padding: 10px;
-  font-size: 0.8rem;
-  cursor: pointer;
-  background-color: transparent;
-  text-transform: capitalize;
-  color: ${(props) => props.theme.primary};
-  border-radius: ${(props) => props.theme.borderRadius};
-  &:hover {
-    background-color: ${(props) => props.theme.backgroundSecondary};
-  }
-`;
-
-const ButtonText = styled.span`
-  margin-left: 10px;
-`;
 
 function PortfoliosList() {
   const [createNewPortfolioModal, setcreateNewPortfolioModal] = useState(false);
   const portfolios = useSelector((state) => state.portfolios.portfolios);
 
   return (
-    <Wrapper>
+    <Flex flexDirection="column">
       <Flex mb="2">
         <Input
           size="sm"
@@ -95,23 +46,32 @@ function PortfoliosList() {
       >
         Portfolios
       </Text>
-      <StyledList>
+      <Stack mb="2" spacing="2">
         {portfolios.map((portfolio) => (
-          <Item key={portfolio.id}>
-            <ItemText>{portfolio.name}</ItemText>
-            <StyledGrab height={16} width={16} />
-          </Item>
+          <Button
+            key={portfolio.id}
+            size="sm"
+            justifyContent="flex-start"
+            variant="ghost"
+          >
+            {portfolio.name}
+          </Button>
         ))}
-      </StyledList>
-      <CreateNewButton onClick={() => setcreateNewPortfolioModal(true)}>
-        <FiPlus />
-        <ButtonText>Create new portfolio</ButtonText>
-      </CreateNewButton>
+      </Stack>
+      <Button
+        size="sm"
+        justifyContent="flex-start"
+        variant="ghost"
+        leftIcon={<FiPlus />}
+        onClick={() => setcreateNewPortfolioModal(true)}
+      >
+        Create new portfolio
+      </Button>
       <CreateNewPortfolioModal
         isOpen={createNewPortfolioModal}
         onClose={() => setcreateNewPortfolioModal(false)}
       />
-    </Wrapper>
+    </Flex>
   );
 }
 
