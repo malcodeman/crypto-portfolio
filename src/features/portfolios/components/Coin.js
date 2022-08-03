2d26b010ffdb1e2d0c1a64e226f797c0e391dff8
malcodeman <26983729+malcodeman@users.noreply.github.com>
2022-08-04 00:12:12 +0200

    Updates coin component

--- src/features/portfolios/components/Coin.js
+++ src/features/portfolios/components/Coin.js
@@ -1,90 +1,40 @@
+import { Box, Flex, Text, useColorModeValue } from "@chakra-ui/react";
 import React from "react";
-import styled from "styled-components";
-
-const Wrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`;
-
-const StyledCoin = styled.div`
-  display: flex;
-  flex-direction: column;
-  padding: 10px;
-  width: 100%;
-  color: ${props => props.theme.primary};
-  border-radius: ${props => props.theme.borderRadius};
-  background-color: ${props => props.theme.backgroundSecondary};
-`;
-
-const Header = styled.header`
-  display: flex;
-  justify-content: space-between;
-  margin-bottom: 10px;
-`;
-
-const Symbol = styled.span`
-  text-transform: uppercase;
-  font-size: 0.8rem;
-  margin-right: 20px;
-`;
-
-const TimePeriod = styled.span`
-  font-size: 0.8rem;
-  text-transform: uppercase;
-  color: ${props => props.theme.secondary};
-`;
-
-const Main = styled.main`
-  display: flex;
-  justify-content: space-between;
-`;
-
-const PriceWrapper = styled.div`
-  font-size: 0.8rem;
-`;
-
-const FiatCurrency = styled.span``;
-
-const Price = styled.span`
-  font-size: 0.8rem;
-  font-weight: 500;
-`;
-
-const PercentChange = styled.span`
-  font-size: 0.8rem;
-  color: ${props =>
-    props.bullish ? props.theme.bullish : props.theme.bearish};
-`;
-
-const Name = styled.span`
-  font-size: 0.8rem;
-  margin-top: 10px;
-`;
 
 function Coin(props) {
-  const { symbol, price, name, percentChange24h } = props;
+  const { symbol, price, percentChange24h } = props;
   const bullish = percentChange24h > 0;
-
+  const bgColor = useColorModeValue(
+    "var(--chakra-colors-gray-100)",
+    "var(--chakra-colors-whiteAlpha-200)"
+  );
   return (
-    <Wrapper>
-      <StyledCoin>
-        <Header>
-          <Symbol>{symbol}</Symbol>
-          <TimePeriod>24h</TimePeriod>
-        </Header>
-        <Main>
-          <PriceWrapper>
-            <FiatCurrency>USD</FiatCurrency> <Price>{price}</Price>
-          </PriceWrapper>
-          <PercentChange bullish={bullish}>
+    <Flex
+      bgColor={bgColor}
+      flexDirection="column"
+      alignItems="center"
+      borderRadius="md"
+    >
+      <Flex flexDirection="column" padding="2" width="full">
+        <Flex justifyContent="space-between" mb="2">
+          <Text fontSize="sm" textTransform="uppercase" mr="2">
+            {symbol}
+          </Text>
+          <Text fontSize="sm" textTransform="uppercase" opacity="0.8">
+            24h
+          </Text>
+        </Flex>
+        <Flex justifyContent="space-between">
+          <Box>
+            <Text>USD</Text> <Text>{price}</Text>
+          </Box>
+          <Text color={bullish ? "green.400" : "red.400"}>
             {bullish && "+"}
             {percentChange24h}%
-          </PercentChange>
-        </Main>
-      </StyledCoin>
-      <Name>{name}</Name>
-    </Wrapper>
+          </Text>
+        </Flex>
+      </Flex>
+    </Flex>
   );
 }
 
