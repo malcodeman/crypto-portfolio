ad24653c4265bbf04ca8484c1f13f5addc3bfc70
malcodeman <26983729+malcodeman@users.noreply.github.com>
2022-09-24 00:12:12 +0200

    Removes redux

--- src/core/state/rootReducer.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "redux";
-
-import portfolios from "../../features/portfolios/reducers/portfoliosReducers";
-
-const rootReducer = combineReducers({
-  portfolios,
-});
-
-export default rootReducer;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
 
 import App from "./core/App";
-import store from "./core/state/store";
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
